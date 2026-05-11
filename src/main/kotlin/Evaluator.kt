5580c85916d587a82515bb4adb8f11a68cc5e819
davlatbekmamadaliev <stevedavlatbek@gmail.com>
2026-05-11 03:34:45 +0200

    ci: add j2k GitHub Actions pipeline

--- src/main/kotlin/Evaluator.kt
+++ src/main/kotlin/Evaluator.kt
@@ -1,8 +1,3 @@
-package org.example
-
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
     println("J2K Evaluation Pipeline - Starting...")
-    // TODO: evaluation logic coming later
 }
